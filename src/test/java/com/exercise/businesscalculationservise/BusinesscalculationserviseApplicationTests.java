de174ace2df12b099a8115885fca6c91a34886b2
asudesozcu <sozcuasude@gmail.com>
2024-08-11 18:58:37 +0300

    Frist Commit

---
+++ src/test/java/com/exercise/businesscalculationservise/BusinesscalculationserviseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.businesscalculationservise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinesscalculationserviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
