b61e713d26ad38bc14ed56365c566f27dec6de76
asudesozcu <sozcuasude@gmail.com>
2024-08-11 19:07:24 +0300

    yorum satırı

--- src/main/java/com/exercise/businesscalculationservise/RealWordlSpringContext.java
+++ src/main/java/com/exercise/businesscalculationservise/RealWordlSpringContext.java
@@ -8,7 +8,7 @@ public class RealWordlSpringContext {
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(RealWordlSpringContext.class)) {
             System.out.println(context.getBean(BusinesscalculationserviseApplication.class).FindMax());
-        }
+        }//
 
     }
 }
